5646f4b9e4d653ef6a5db6293d28812bfb891064
jip <jpbfrancisco199@gmail.com>
2019-11-03 19:00:51 -0800

    auxillary edits

--- visualize.py
+++ visualize.py
@@ -1,38 +1,41 @@
 import json
 import sys
 
-def writeToFrequencyFile(mostFrequent, frequency_file_name):
+def writeToFrequencyFile(mostFrequent, frequency_file_name, participants):
     try:
         file = open(frequency_file_name + ".csv", "wb")
-        file.write("word,total,".encode("utf-8"))
-        for participant in data["participants"]:
-            file.write((participant["name"] + ",").encode("utf-8"))
-        file.write("\n".encode("utf-8"))
-
-        for mostUsedWord in mostFrequent:
-            file.write((mostUsedWord[0] + ",").encode("utf-8"))
-            file.write((str(mostUsedWord[1]["total"]) + ",").encode("utf-8"))
-            for participant in data["participants"]:
-                if (participant["name"] in mostUsedWord[1]):
-                    file.write((str(mostUsedWord[1][participant["name"]]) + ",").encode("utf-8"))
-            file.write("\n".encode("utf-8"))
+        headers = "word,total,"
+        for participant in participants:
+            headers += participant["name"] + ","
+        headers += "\n"
+        file.write(headers.encode("utf-8"))
         
+        body = ""
+        for mostUsedWord in mostFrequent:
+            body += mostUsedWord[0] + ","
+            body += str(mostUsedWord[1]["total"]) + ","
+            for participant in participants:
+                if (participant["name"] in mostUsedWord[1]):
+                    body += str(mostUsedWord[1][participant["name"]]) + ","
+            body += "\n"
+        file.write(body.encode("utf-8"))
         file.close()
-    except:
+    except Exception as e:
         print("An error occured while opening or writing to file: " + frequency_file_name)
+        print(str(e))
 
 def writeToSenderFrequencyFile(senders, sender_file_name):
     try:
         file2 = open(sender_file_name + ".csv", "wb")
         for sender in list(senders.items()):
             file2.write((sender[0] + "," + str(sender[1]) + "\n").encode("utf-8"))
-        
         file2.close()
-    except: 
+    except Exception as e: 
         print("An error occured while opening or writing to file: " + sender_file_name)
+        print(str(e))
 
-def writeToFiles(mostFrequent, senders, frequency_file_name, sender_file_name):
-    writeToFrequencyFile(mostFrequent, frequency_file_name)
+def writeToFiles(mostFrequent, senders, participants, frequency_file_name, sender_file_name):
+    writeToFrequencyFile(mostFrequent, frequency_file_name, participants)
     writeToSenderFrequencyFile(senders, sender_file_name)
 
     """
@@ -77,23 +80,27 @@ def processFiles(data, start, end, frequency_file_name, sender_file_name):
     try: 
         populateFrequencyDicts(data, mostFrequent, senders)
         mostUsed = getMostUsed(mostFrequent, start, end)
-        writeToFiles(mostUsed, senders, frequency_file_name, sender_file_name)
+        writeToFiles(mostUsed, senders, data["participants"], frequency_file_name, sender_file_name)
     except Exception as e: 
         print("An error occured while extracting data from the file, make sure you are using the right file provided by facebook in json format")
         print(str(e))
-        
-if (len(sys.argv) == 4 or len(sys.argv) == 6): 
-    start = 0
-    end = 1000
-    if (len(sys.argv) == 6):
-        start = sys.argv[4]
-        end = sys.argv[5]
-    try:
-        file = open(sys.argv[1])
-        data = json.load(file)
-        file.close()
-        processFiles(data, start, end, sys.argv[2], sys.argv[3])
-    except:
-        print("Something went wrong while opening " + sys.argv[1])
-else:
-    print("Invalid argument list. Script should be called with py visualize.py [input_file] [frequency_file_name] [sender_file_name] OPTIONAL[start] OPTIONAL[end]")
+
+def main():     
+    if (len(sys.argv) == 4 or len(sys.argv) == 6): 
+        start = 0
+        end = 1000
+        if (len(sys.argv) == 6):
+            start = sys.argv[4]
+            end = sys.argv[5]
+        try:
+            file = open(sys.argv[1])
+            data = json.load(file)
+            file.close()
+            processFiles(data, start, end, sys.argv[2], sys.argv[3])
+        except Exception as e:
+            print("Something went wrong while opening " + sys.argv[1])
+    else:
+        print("Invalid argument list. Script should be called with py visualize.py [input_file] [frequency_file_name] [sender_file_name] OPTIONAL[start] OPTIONAL[end]")
+
+if __name__ == "__main__":
+    main()
